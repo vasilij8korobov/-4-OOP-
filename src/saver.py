f7285c494923a827d9d34d25ef59620da51e5213
vasilij korobov <kavyabass51@gmail.com>
2024-07-21 23:07:22 +0700

    делаем логику в пакете src

---
+++ src/saver.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Saver(ABC):
+    """Абстрактный класс для записи в файл"""
+    def __init__(self, silename):
+        self.filename = filename
+
+    @abstractmethod
+    def write_data(self, vacancies):
+        pass
+
+    @abstractmethod
+    def get_data(self):
+        pass
+
+    @abstractmethod
+    def del_data(self):
+        pass
