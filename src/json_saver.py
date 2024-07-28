0a1a1276983a4df93f8edae79e0b0277991a7bea
vasilij korobov <kavyabass51@gmail.com>
2024-07-28 12:59:00 +0700

    Атрибуты экземпляров изменены на зашищенные

--- src/json_saver.py
+++ src/json_saver.py
@@ -17,19 +17,19 @@ class JSONSaver(Saver):
         data = self.get_data()
         data.extend(vacancies)
 
-        with open(self.filename, "w", encoding="utf-8") as file:
+        with open(self._filename, "w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
     def get_data(self):
         """ Получение данных json """
 
         try:
-            return json.load(open(self.filename, encoding="utf-8"))
+            return json.load(open(self._filename, encoding="utf-8"))
         except FileNotFoundError:
             return []
 
     def del_data(self):
         """ Удаление данных из файла """
 
-        with open(self.filename, "w", encoding="utf-8") as file:
+        with open(self._filename, "w", encoding="utf-8") as file:
             json.dump([], file, ensure_ascii=False, indent=4)
