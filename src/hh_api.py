d7f7c45244caa42796868b74018f5f6b2daa356f
vasilij korobov <kavyabass51@gmail.com>
2024-07-28 12:45:13 +0700

    hh_api.py теперь приватный

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,5 +1,5 @@
 import requests
-
+from requests import Response
 from src.get_vacancies import GetVacanciesAPI
 
 
@@ -7,15 +7,14 @@ class HeadHunterAPI(GetVacanciesAPI):
     """класс для подключения к HH.ru"""
 
     def __init__(self):
-        self.url = "https://api.hh.ru/vacancies"
-        self.headers = {"User-Agent": "HH-User-Agent"}
-        self.params = {"text": "", "per_page": "", "only-with_salary": True}
+        self.__url = "https://api.hh.ru/vacancies"
+        self.__headers = {"User-Agent": "HH-User-Agent"}
+        self.__params = {"text": "", "per_page": "", "only_with_salary": True}
 
     def get_response(self, keyword, per_page):
-        self.params["text"] = keyword
-        self.params["per_page"] = per_page
-        return requests.get(self.url, params=self.params)
+        self.__params["text"] = keyword
+        self.__params["per_page"] = per_page
+        return requests.get(self.__url, params=self.__params)
 
     def get_vacancies(self, keyword, per_page):
         return self.get_response(keyword, per_page).json()["items"]
-
